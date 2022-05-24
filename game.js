1fca6628029b19c3ebacef51c0f08c75fdafbb45
Ahmad Othman <aothman156@gmail.com>
2022-05-24 12:18:15 +0300

    tmier added

--- game.js
+++ game.js
@@ -1,11 +1,17 @@
 window.onload=function(){
     let scoreTag = '';
+    let end_time=0;
+    let best_min = 0;
+    let best_sec =0;
+    let won = 0;
     createScore();
     createRestartButton();
+    createTime();
     let statusH2 = document.getElementById("status");
     let bounds = document.getElementsByClassName("boundary");
 
     function startGame(){
+        won = 0;
         statusH2.innerHTML = "Game Started";
         document.getElementById("end").addEventListener('mouseover',youWon);
         console.log("game starts");
@@ -36,15 +42,19 @@ window.onload=function(){
         updateScoreDown();
         statusH2.innerHTML = "You Lost!";
 
+
          
     }
     function youWon(){
+        won =1 ;
         removeWallsListener();
         document.getElementById("end").removeEventListener('mouseover',youWon);
         document.getElementById("game").removeEventListener('mouseleave',cheating);
         makeDefault();
         updateScoreUp();
         statusH2.innerHTML = "You Won!";
+        end_time = 1;
+
 
     }
     function cheating(){
@@ -53,7 +63,7 @@ window.onload=function(){
         document.getElementById("end").removeEventListener('mouseover',youWon);
         removeWallsListener();
         statusH2.innerHTML = "Begin by moving your mouse over the \"S\".";
-        
+        end_time = 1;
 
      }
      function removeWallsListener(){
@@ -67,6 +77,7 @@ window.onload=function(){
         localStorage.setItem('score',newScore);
         score = localStorage.getItem('score');
         scoreTag.innerHTML = "Score : " + score;
+
      }
      function updateScoreDown(){
         let newScore = parseInt(localStorage.getItem('score'));
@@ -74,10 +85,13 @@ window.onload=function(){
         localStorage.setItem('score',newScore);
         score = localStorage.getItem('score');
         scoreTag.innerHTML = "Score : " + score;
+        end_time = 1;
+
      }
      function restart(){
         localStorage.setItem('score',0);
         document.querySelector("#score_id").innerHTML = "Score : 0";
+        end_time = 1;
      }
 
      function createRestartButton(){
@@ -101,5 +115,71 @@ window.onload=function(){
         document.getElementsByTagName('body')[0].appendChild(scoreTag);
         scoreTag.innerHTML = "Score : " + score;
      }
-        document.getElementById("start").addEventListener('click',startGame);
+     function createTime(){
+        let timeElapsed = document.createElement("div");
+        let button = document.querySelector('button').parentNode;
+        button.parentNode.insertBefore(timeElapsed,button);
+        timeElapsed.innerHTML = "<div id ='live'></div><div id ='last'></div><div id ='best'></div>"
+        timeElapsed.style.display = 'flex';
+        timeElapsed.style.justifyContent = 'center';
+        timeElapsed.style.margin = ' 30px';
+        timeElapsed.style.fontSize = ' 25px';
+        timeElapsed.childNodes[0].style.margin = '0 100px 0 100px';
+        timeElapsed.childNodes[1].style.margin = '0 100px 0 100px';
+        timeElapsed.childNodes[2].style.margin = '0 100px 0 100px';
+        document.getElementById('live').innerHTML = "Live: 0:0.0";
+        document.getElementById('last').innerHTML = "Last: 0:0.0";
+        document.getElementById('best').innerHTML = "Best: 0:0.0";
+
+
+
+     }
+     function startTimer(minutes = 0){
+         end_time = 0; 
+         let start_time = Date.now();
+         console.log(start_time);
+         let milliseconds = 0;
+         let seconds = 0;
+         var interval = setInterval(timer,10);
+         function timer(){
+            elapsed_time= Date.now() - start_time;
+            milliseconds = Math.floor(elapsed_time) ;
+            seconds =  (milliseconds /1000).toFixed(1);
+            if (milliseconds > 60000){
+                minutes++;
+                milliseconds = 0;
+                seconds = 0;
+                clearInterval(interval);
+                startTimer(minutes);
+                
+            }
+            document.getElementById('live').innerHTML = "Live: " + minutes + ":" + seconds;
+         
+         if(end_time === 1 ){
+            clearInterval(interval);
+            if(won === 1){
+                document.getElementById('last').innerHTML = "Last: " + minutes + ":" + seconds;
+                document.getElementById('live').innerHTML = "Live: 0:0.0";
+                if(best_min === 0 && best_sec ===0 ){
+                    best_min= minutes;
+                    best_sec = seconds;
+                    document.getElementById('best').innerHTML = "Best: " + best_min + ":" + best_sec;
+                }
+                else{
+                    if(minutes<=best_min){
+                        if(seconds<best_sec){
+                            best_min= minutes;
+                            best_sec = seconds;
+                            document.getElementById('best').innerHTML = "Best: " + best_min + ":" + best_sec;
+                        }
+                    }
+                }
+
+         }
+        }
+    }
+     }
+     
+
+        document.getElementById("start").addEventListener('mouseover',function(){startGame();startTimer();});
 };
